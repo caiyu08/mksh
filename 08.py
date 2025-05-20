df8de5fd62daa4c71b2fac266c3d94ba00b8553f
caiyu08 <asd980128666@gmail.com>
2025-05-20 16:15:57 +0800

    Add files via upload
    5/20

--- 08.py
+++ 08.py
@@ -1,3 +1,6 @@
-list_data=[1,2,3,4,5]
-list_data=list_data*3
-print(list_data)
+data="76 98 42 90 80 62"
+data=data.split()
+scores=list()
+for item in data:
+    scores.append(int(item))
+print(scores)
